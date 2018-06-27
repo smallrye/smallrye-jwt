a5aa4d916ce26cd9238cb4c17c69e82d3ab3115e
Scott Stark <starksm64@gmail.com>
2018-06-26 20:55:34 -0700

    Initial checkpoint of principal and config classes

---
+++ implementation/src/main/java/io/smallrye/jwt/auth/principal/ParseException.java
@@ -0,0 +1,32 @@
+/**
+ *
+ *   Copyright 2017 Red Hat, Inc, and individual contributors.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package io.smallrye.jwt.auth.principal;
+
+/**
+ * The exception thrown when there is a failure to parse the JWT
+ */
+public class ParseException extends Exception {
+    private static final long serialVersionUID = 1L;
+
+    public ParseException(String message) {
+        super(message);
+    }
+
+    public ParseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
