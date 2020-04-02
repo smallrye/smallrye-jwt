af6d031c6970c0d7704c448387a02e7eff24c577
Roberto Cortez <radcortez@yahoo.com>
2020-04-02 13:24:15 +0100

    #80 - Added configuration to validate required claims in the JWT. (#209)

---
+++ testsuite/basic/src/test/java/io/smallrye/jwt/TestTokenRequiredClaims.java
@@ -0,0 +1,77 @@
+package io.smallrye.jwt;
+
+import static java.util.stream.Collectors.toSet;
+import static org.eclipse.microprofile.jwt.tck.TCKConstants.TEST_ISSUER;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Collections;
+import java.util.stream.Stream;
+
+import org.eclipse.microprofile.jwt.tck.util.TokenUtils;
+import org.jose4j.jwt.consumer.InvalidJwtException;
+import org.junit.Test;
+
+import io.smallrye.jwt.auth.principal.JWTAuthContextInfo;
+import io.smallrye.jwt.auth.principal.JWTCallerPrincipalFactory;
+import io.smallrye.jwt.auth.principal.ParseException;
+
+public class TestTokenRequiredClaims {
+    @Test
+    public void base() throws Exception {
+        String token = TokenUtils.generateTokenString("/Token1.json");
+        PublicKey publicKey = TokenUtils.readPublicKey("/publicKey.pem");
+        JWTAuthContextInfo contextInfo = new JWTAuthContextInfo((RSAPublicKey) publicKey, TEST_ISSUER);
+        JWTCallerPrincipalFactory factory = JWTCallerPrincipalFactory.instance();
+        factory.parse(token, contextInfo);
+    }
+
+    @Test
+    public void missingRequiredClaim() throws Exception {
+        String token = TokenUtils.generateTokenString("/Token1.json");
+        PublicKey publicKey = TokenUtils.readPublicKey("/publicKey.pem");
+        JWTAuthContextInfo contextInfo = new JWTAuthContextInfo((RSAPublicKey) publicKey, TEST_ISSUER);
+        contextInfo.setRequiredClaims(Collections.singleton("something"));
+        JWTCallerPrincipalFactory factory = JWTCallerPrincipalFactory.instance();
+
+        final ParseException exception = assertThrows(ParseException.class, () -> factory.parse(token, contextInfo));
+        assertTrue(exception.getCause() instanceof InvalidJwtException);
+    }
+
+    @Test
+    public void missingRequiredClaims() throws Exception {
+        String token = TokenUtils.generateTokenString("/Token1.json");
+        PublicKey publicKey = TokenUtils.readPublicKey("/publicKey.pem");
+        JWTAuthContextInfo contextInfo = new JWTAuthContextInfo((RSAPublicKey) publicKey, TEST_ISSUER);
+        contextInfo.setRequiredClaims(Stream.of("something", "else").collect(toSet()));
+        JWTCallerPrincipalFactory factory = JWTCallerPrincipalFactory.instance();
+
+        final ParseException exception = assertThrows(ParseException.class, () -> factory.parse(token, contextInfo));
+        assertTrue(exception.getCause() instanceof InvalidJwtException);
+    }
+
+    @Test
+    public void requiredClaims() throws Exception {
+        String token = TokenUtils.generateTokenString("/Token1.json");
+        PublicKey publicKey = TokenUtils.readPublicKey("/publicKey.pem");
+        JWTAuthContextInfo contextInfo = new JWTAuthContextInfo((RSAPublicKey) publicKey, TEST_ISSUER);
+        contextInfo.setRequiredClaims(Stream.of("roles", "customObject", "customDoubleArray").collect(toSet()));
+        JWTCallerPrincipalFactory factory = JWTCallerPrincipalFactory.instance();
+        factory.parse(token, contextInfo);
+    }
+
+    @Test
+    public void requiredAndMissingClaims() throws Exception {
+        String token = TokenUtils.generateTokenString("/Token1.json");
+        PublicKey publicKey = TokenUtils.readPublicKey("/publicKey.pem");
+        JWTAuthContextInfo contextInfo = new JWTAuthContextInfo((RSAPublicKey) publicKey, TEST_ISSUER);
+        contextInfo.setRequiredClaims(
+                Stream.of("roles", "customObject", "customDoubleArray", "something").collect(toSet()));
+        JWTCallerPrincipalFactory factory = JWTCallerPrincipalFactory.instance();
+
+        final ParseException exception = assertThrows(ParseException.class, () -> factory.parse(token, contextInfo));
+        assertTrue(exception.getCause() instanceof InvalidJwtException);
+    }
+}
