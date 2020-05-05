42d3d576ef3e6665faab30547bb894ead210ee91
sberyozkin <sberyozkin@gmail.com>
2020-05-05 22:49:30 +0100

    Add a method to init JwitClaimsBuilder from JsonWebToken (#229)

--- implementation/src/main/java/io/smallrye/jwt/build/impl/JwtProviderImpl.java
+++ implementation/src/main/java/io/smallrye/jwt/build/impl/JwtProviderImpl.java
@@ -1,6 +1,10 @@
 package io.smallrye.jwt.build.impl;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
+
+import org.eclipse.microprofile.jwt.Claims;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
 import io.smallrye.jwt.build.JwtClaimsBuilder;
 import io.smallrye.jwt.build.spi.JwtProvider;
@@ -33,6 +37,21 @@ public class JwtProviderImpl extends JwtProvider {
     @Override
     public JwtClaimsBuilder claims(String jsonLocation) {
         return new JwtClaimsBuilderImpl(jsonLocation);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public JwtClaimsBuilder claims(JsonWebToken jwt) {
+        Map<String, Object> claims = new LinkedHashMap<>();
+        for (String name : jwt.getClaimNames()) {
+            if (Claims.raw_token.name().equals(name)) {
+                continue;
+            }
+            claims.put(name, jwt.getClaim(name));
+        }
+        return claims(claims);
     }
 
 }
