575b2ec23e3d31875cceb6d33fe79ca9a734dc05
Roberto Cortez <radcortez@yahoo.com>
2020-05-20 19:26:49 +0100

    Fixed compile issues.

--- testsuite/basic/src/test/java/io/smallrye/jwt/TestTokenRequireSub.java
+++ testsuite/basic/src/test/java/io/smallrye/jwt/TestTokenRequireSub.java
@@ -3,12 +3,12 @@ package io.smallrye.jwt;
 import static org.eclipse.microprofile.jwt.tck.TCKConstants.TEST_GROUP_JWT;
 import static org.eclipse.microprofile.jwt.tck.TCKConstants.TEST_ISSUER;
 
-import java.security.PublicKey;
 import java.security.interfaces.RSAPublicKey;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.eclipse.microprofile.jwt.tck.util.SignatureAlgorithm;
 import org.eclipse.microprofile.jwt.tck.util.TokenUtils;
 import org.jboss.arquillian.testng.Arquillian;
 import org.testng.Assert;
@@ -22,13 +22,13 @@ public class TestTokenRequireSub extends Arquillian {
     @Test(groups = TEST_GROUP_JWT, description = "validate sub")
     public void defaultSubAvailable() throws Exception {
         Map<String, Long> timeClaims = new HashMap<>();
-        String token = TokenUtils.signClaims("/Token1.json", null, timeClaims);
-        PublicKey publicKey = TokenUtils.readPublicKey("/publicKey.pem");
+        String token = TokenUtils.signClaims("/Token1.json", SignatureAlgorithm.RS256, null, timeClaims);
+        RSAPublicKey publicKey = TokenUtils.readPublicKey("/publicKey.pem");
         if (publicKey == null) {
             throw new IllegalStateException("Failed to load /publicKey.pem resource");
         }
 
-        JWTAuthContextInfo contextInfo = new JWTAuthContextInfo((RSAPublicKey) publicKey, TEST_ISSUER);
+        JWTAuthContextInfo contextInfo = new JWTAuthContextInfo(publicKey, TEST_ISSUER);
         JWTCallerPrincipalFactory factory = JWTCallerPrincipalFactory.instance();
         JsonWebToken jwt = factory.parse(token, contextInfo);
         String sub = jwt.getSubject();
@@ -38,13 +38,13 @@ public class TestTokenRequireSub extends Arquillian {
     @Test(groups = TEST_GROUP_JWT, description = "validate sub fail", expectedExceptions = ParseException.class)
     public void defaultSubNotAvailable() throws Exception {
         Map<String, Long> timeClaims = new HashMap<>();
-        String token = TokenUtils.signClaims("/TokenSubPath.json", null, timeClaims);
-        PublicKey publicKey = TokenUtils.readPublicKey("/publicKey.pem");
+        String token = TokenUtils.signClaims("/TokenSubPath.json", SignatureAlgorithm.RS256, null, timeClaims);
+        RSAPublicKey publicKey = TokenUtils.readPublicKey("/publicKey.pem");
         if (publicKey == null) {
             throw new IllegalStateException("Failed to load /publicKey.pem resource");
         }
 
-        JWTAuthContextInfo contextInfo = new JWTAuthContextInfo((RSAPublicKey) publicKey, TEST_ISSUER);
+        JWTAuthContextInfo contextInfo = new JWTAuthContextInfo(publicKey, TEST_ISSUER);
         JWTCallerPrincipalFactory factory = JWTCallerPrincipalFactory.instance();
         factory.parse(token, contextInfo);
     }
@@ -52,13 +52,13 @@ public class TestTokenRequireSub extends Arquillian {
     @Test(groups = TEST_GROUP_JWT, description = "no sub validation")
     public void noSubValidation() throws Exception {
         Map<String, Long> timeClaims = new HashMap<>();
-        String token = TokenUtils.signClaims("/TokenSubPath.json", null, timeClaims);
-        PublicKey publicKey = TokenUtils.readPublicKey("/publicKey.pem");
+        String token = TokenUtils.signClaims("/TokenSubPath.json", SignatureAlgorithm.RS256, null, timeClaims);
+        RSAPublicKey publicKey = TokenUtils.readPublicKey("/publicKey.pem");
         if (publicKey == null) {
             throw new IllegalStateException("Failed to load /publicKey.pem resource");
         }
 
-        JWTAuthContextInfo contextInfo = new JWTAuthContextInfo((RSAPublicKey) publicKey, TEST_ISSUER);
+        JWTAuthContextInfo contextInfo = new JWTAuthContextInfo(publicKey, TEST_ISSUER);
         contextInfo.setRequireNamedPrincipal(false);
         JWTCallerPrincipalFactory factory = JWTCallerPrincipalFactory.instance();
         JsonWebToken jwt = factory.parse(token, contextInfo);
