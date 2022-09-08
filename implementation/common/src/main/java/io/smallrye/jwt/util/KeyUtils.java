c5642a737b742bb861f88e8802f955caeedfbe23
sberyozkin <sberyozkin@gmail.com>
2022-09-08 13:02:52 +0100

    Fail loading the keystore if no configured provider is found (#640)

--- implementation/common/src/main/java/io/smallrye/jwt/util/KeyUtils.java
+++ implementation/common/src/main/java/io/smallrye/jwt/util/KeyUtils.java
@@ -285,7 +285,13 @@ public final class KeyUtils {
             Optional<String> keyStoreProvider)
             throws Exception {
         String theKeyStoreType = getKeyStoreType(keyStorePath, keyStoreType);
-        Provider provider = keyStoreProvider.isPresent() ? Security.getProvider(keyStoreProvider.get()) : null;
+        Provider provider = null;
+        if (keyStoreProvider.isPresent()) {
+            provider = Security.getProvider(keyStoreProvider.get());
+            if (provider == null) {
+                throw JWTUtilMessages.msg.keystoreProviderNotFound(keyStoreProvider.get());
+            }
+        }
         KeyStore keyStore = provider != null
                 ? KeyStore.getInstance(theKeyStoreType, provider)
                 : KeyStore.getInstance(theKeyStoreType);
